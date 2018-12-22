4ccc1dc31ff74b555ebfb3c7d232d12039b76753
docwuqihao <docwuqihao@qq.com>
2018-12-22 16:48:28 +0800

    123

--- src/main/java/com/wqh/boot/controller/BootController.java
+++ src/main/java/com/wqh/boot/controller/BootController.java
@@ -1,14 +1,12 @@
 package com.wqh.boot.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BootController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/hello")
     public String hello(){
         return "Hello world.";
     }
