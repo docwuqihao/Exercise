726db25ec0b5e5b76a26af91409715f2d1055021
docwuqihao <docwuqihao@qq.com>
2018-12-24 10:27:07 +0800

    添加annotation理解

--- src/main/java/com/wqh/boot/controller/BootController.java
+++ src/main/java/com/wqh/boot/controller/BootController.java
@@ -1,13 +1,19 @@
 package com.wqh.boot.controller;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.wqh.boot.vo.Message;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/boot")
 public class BootController {
+    private static AtomicInteger count = new AtomicInteger(0);
 
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello world.";
+    public Message hello(@RequestParam(name = "name", defaultValue = "world") String name) {
+        return new Message(count.incrementAndGet(), String.format("Hello %s.", name));
     }
 }
